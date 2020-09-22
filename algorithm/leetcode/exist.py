1b45e224aedcf687d5f952c38cb3d795484ef8bf
panmeng <panm9@mail2.sysu.edu.cn>
2020-09-22 15:42:09 +0800

    updata exist

--- algorithm/leetcode/exist.py
+++ algorithm/leetcode/exist.py
@@ -1,33 +1,21 @@
 List = [[]]
 class Solution:
 	def exist(self, board: List[List[str]], word: str) -> bool:
-
-		def help(board, x, y, word, i, visited):
-			if i >= len(word):
+		def find(i,j,k):
+			if not 0 <= i <len(board) or 0 <= j < len(board[0]) or board[i][j] != word[k]:
+				return False
+			if k == len(word) - 1:
 				return True
+			tmp,board[i][j] = board[i][j],'/'
+			result = find(i,j-1,k+1) or find(i,j+1,k+1) or find(i-1,j,k+1) or find(i+1,j,k+1)
+			board[i][j] = tmp
+			return result
 
-			if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
-				return False
-
-			if visited[x][y]:
-				return False
-
-			if board[x][y] != word[i]:
-				return False
-
-			visited[x][y] = True
-
-			# print(x, y)
-
-			res = help(board, x-1, y, word, i+1, visited) or help(board, x+1, y, word, i+1, visited) or help(board, x, y-1, word, i+1, visited) or help(board, x, y+1, word, i+1, visited)
-			if not res:
-				visited[x][y] = False
-			return res
-
-		for i in range(len(board)):
-			for j in range(len(board[0])):
+		for i in range(board):
+			for j in range(board[0]):
 				if board[i][j] == word[0]:
-					visited = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-					if help(board, i, j, word, 0, visited):
+					if find(i,j,0):
 						return True
 		return False
+
+
