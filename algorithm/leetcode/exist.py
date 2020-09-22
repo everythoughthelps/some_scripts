fa8661dff1e66d6d5f3b011408e5bd3fb7e83d55
panmeng <panm9@mail2.sysu.edu.cn>
2020-09-22 20:42:02 +0800

    update exist

--- algorithm/leetcode/exist.py
+++ algorithm/leetcode/exist.py
@@ -2,7 +2,7 @@ List = [[]]
 class Solution:
 	def exist(self, board: List[List[str]], word: str) -> bool:
 		def find(i,j,k):
-			if not 0 <= i <len(board) or 0 <= j < len(board[0]) or board[i][j] != word[k]:
+			if not 0 <= i <len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]:
 				return False
 			if k == len(word) - 1:
 				return True
@@ -11,8 +11,8 @@ class Solution:
 			board[i][j] = tmp
 			return result
 
-		for i in range(board):
-			for j in range(board[0]):
+		for i in range(len(board)):
+			for j in range(len(board[0])):
 				if board[i][j] == word[0]:
 					if find(i,j,0):
 						return True
